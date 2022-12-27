ef3fe4a688f41ebfd604ac840f81ee50d253af2a
Andrei Alecu <andreialecu@users.noreply.github.com>
2022-12-27 12:04:03 +0200

    repro: tailwind jit not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', '../tailwind.css']
 })
 export class AppComponent {
   title = 'angular-tw-jit-repro';
